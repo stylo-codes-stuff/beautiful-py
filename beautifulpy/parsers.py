99c88ad619ab5ee23c868e58d50743f9bfec2370
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-03-05 19:26:46 +0000

    rewrote js ebnf. The name rule can be used for classes functions and others

--- beautifulpy/parsers.py
+++ beautifulpy/parsers.py
@@ -4,7 +4,7 @@ from lark.indenter import PythonIndenter
 import rich
 # Official Python grammar by Lark
 python = Lark.open_from_package('lark', 'python.lark', ['grammars'],parser='lalr', postlex=PythonIndenter(), start='file_input',maybe_placeholders=False)
-javascript = Lark.open("beautifulpy/grammars/javascript.lark",start="file_input")
+javascript = Lark.open("beautifulpy/grammars/javascript.lark",start="file_input", parser="lalr")
 html = Lark.open("beautifulpy/grammars/html.lark",start="file_input")
 css = Lark.open("beautifulpy/grammars/css.lark",start="file_input")
 with open("tests/javascript_test.js","r") as file:
