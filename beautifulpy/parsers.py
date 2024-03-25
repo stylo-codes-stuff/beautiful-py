8327752fa688b7f02aa5d0b61783c60c87e779ff
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-03-25 21:04:46 +0000

    grammar progress

--- beautifulpy/parsers.py
+++ beautifulpy/parsers.py
@@ -5,7 +5,9 @@ import rich
 # Official Python grammar by Lark
 python = Lark.open_from_package('lark', 'python.lark', ['grammars'],parser='lalr', postlex=PythonIndenter(), start='file_input',maybe_placeholders=False)
 javascript = Lark.open("beautifulpy/grammars/javascript.lark",start="file_input", parser="lalr")
-html = Lark.open("beautifulpy/grammars/html.lark",start="file_input")
-css = Lark.open("beautifulpy/grammars/css.lark",start="file_input")
+html = Lark.open("beautifulpy/grammars/html.lark",start="file_input",parser="lalr")
+css = Lark.open("beautifulpy/grammars/css.lark",start="file_input",parser="lalr")
+mini_js = Lark.open("beautifulpy/grammars/javascript_mincode.lark",start="file_input",parser="lalr")
 with open("tests/javascript_test.js","r") as file:
     rich.print(javascript.parse(file.read()))
+
