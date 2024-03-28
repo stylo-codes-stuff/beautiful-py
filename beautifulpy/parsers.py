9ee1b467ecd4db1c4c088165f45258641dda0c49
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-03-28 20:06:27 +0000

    created larkfiles for esolangs cloned a big folder full of js test files and made progress on js grammar

--- beautifulpy/parsers.py
+++ beautifulpy/parsers.py
@@ -1,13 +1,18 @@
 from lark import Lark
-
+import os
 from lark.indenter import PythonIndenter
 import rich
 # Official Python grammar by Lark
 python = Lark.open_from_package('lark', 'python.lark', ['grammars'],parser='lalr', postlex=PythonIndenter(), start='file_input',maybe_placeholders=False)
-javascript = Lark.open("beautifulpy/grammars/javascript.lark",start="file_input", parser="lalr")
+ECMAscript = Lark.open("beautifulpy/grammars/javascriptECMA.lark",start="file_input", parser="lalr")
 html = Lark.open("beautifulpy/grammars/html.lark",start="file_input",parser="lalr")
 css = Lark.open("beautifulpy/grammars/css.lark",start="file_input",parser="lalr")
-mini_js = Lark.open("beautifulpy/grammars/javascript_mincode.lark",start="file_input",parser="lalr")
-with open("tests/javascript_test.js","r") as file:
-    rich.print(javascript.parse(file.read()))
+mini_ECMAscript = Lark.open("beautifulpy/grammars/javascriptECMA_mincode.lark",start="file_input",parser="lalr")
+for filename in os.listdir("/workspaces/beautiful-py/tests/test262-parser-tests/pass"):
+    f = os.path.join("/workspaces/beautiful-py/tests/test262-parser-tests/pass", filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        with open(f,"r") as file:
+            print(file)
+            rich.print(ECMAscript.parse(file.read()))
 
