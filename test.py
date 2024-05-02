031f0368a6a2f9865be390981a94b124bc179506
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-05-02 21:05:19 +0000

    worked on reformatting the ECMAscript EBNF

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-variable = 1
-class test:
+grammar = open("/workspaces/beautiful-py/beautifulpy/grammars/javascriptECMA.lark","r")
+txt_file = open("test.txt","w")
+import re
+text = grammar.read()
+test = ""
+print(re.search("\"{\"",text))
+matches = re.finditer("{",text)
 
-turt = test()
-import inspect
-print(inspect.isclass(type(turt)))
+match_indexes = [(m.start(0), m.end(0)) for m in re.finditer("{",text)]
+quoted_match_indexes = [(m.start(0), m.end(0)) for m in re.finditer("\"{\"",text)]
+print(quoted_match_indexes)
+for match in match_indexes:
+    print(match)
