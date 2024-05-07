c4b41f317537cc38438ae5a39b1593d40069efb3
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-05-07 02:03:15 +0000

    Added loads of brand new terminals and rules such as a rule for parsing obejct literals

--- beautifulpy/parsers.py
+++ beautifulpy/parsers.py
@@ -6,16 +6,17 @@ import rich
 #test = Lark.open("beautifulpy/grammars/test.lark",parser="lalr")
 
 # Official Python grammar by Lark
-#python = Lark.open_from_package('lark', 'python.lark', ['grammars'],parser='lalr', postlex=PythonIndenter(), start='file_input',maybe_placeholders=False)
+python = Lark.open_from_package('lark', 'python.lark', ['grammars'],parser='lalr', postlex=PythonIndenter(), start='file_input',maybe_placeholders=False)
 #reminder that the keep all tokens param keeps tokens generated from using predifined strings inside of rules such as ones for curly braces and commas
 ECMAscript = Lark.open("beautifulpy/grammars/javascriptECMA.lark",start="file_input", parser="lalr",keep_all_tokens=True,debug=True)
-#html = Lark.open("beautifulpy/grammars/html.lark",start="file_input",parser="lalr")
-#css = Lark.open("beautifulpy/grammars/css.lark",start="file_input",parser="lalr")
-#for filename in os.listdir("/workspaces/beautiful-py/tests/test262-parser-tests/pass"):
-#    f = os.path.join("/workspaces/beautiful-py/tests/test262-parser-tests/pass", filename)
-#    # checking if it is a file
-#    if os.path.isfile(f):
-#        with open(f,"r") as file:
-#            tokens = ECMAscript.parse(file.read())
-#            time.sleep(.2)
-#            rich.print(tokens)
+html = Lark.open("beautifulpy/grammars/html.lark",start="file_input",parser="lalr")
+css = Lark.open("beautifulpy/grammars/css.lark",start="file_input",parser="lalr")
+for filename in os.listdir("/workspaces/beautiful-py/tests/test262-parser-tests/pass"):
+    f = os.path.join("/workspaces/beautiful-py/tests/test262-parser-tests/pass", filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        with open(f,"r") as file:
+            print(file)
+            tokens = ECMAscript.parse(file.read())
+            time.sleep(.2)
+            rich.print(tokens)
