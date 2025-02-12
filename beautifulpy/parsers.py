8bae27a59cc8a27a73717c34fd8b724960533a6a
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2025-02-12 19:42:38 +0000

    finally created a functional tokenizer for javascript

--- beautifulpy/parsers.py
+++ beautifulpy/parsers.py
@@ -4,7 +4,7 @@ from lark.indenter import PythonIndenter
 import rich
 # Official Python grammar by Lark
 python = Lark.open_from_package('lark', 'python.lark', ['grammars'],parser='lalr', postlex=PythonIndenter(), start='file_input',maybe_placeholders=False)
-javascript = Lark.open("beautifulpy/grammars/javascript.lark",start="file_input", parser="lalr")
+javascript = Lark.open("beautifulpy/grammars/jstokens.lark",start="file_input", parser="lalr")
 html = Lark.open("beautifulpy/grammars/html.lark",start="file_input")
 css = Lark.open("beautifulpy/grammars/css.lark",start="file_input")
 #mini_js = Lark.open("beautifulpy/grammars/javascript.lark")
